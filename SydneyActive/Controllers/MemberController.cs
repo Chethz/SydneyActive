86cf96ebac446ade2a738ced4f066e14a4e4f11c
Chethz <chethzz@hotmail.com>
2022-06-24 16:32:03 +1000

    Added Index action to Member controller.

---
+++ SydneyActive/Controllers/MemberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SydneyActive.Data;
+using SydneyActive.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SydneyActive.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MemberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Member> objMemberList = _db.Members.ToList();
+            return View(objMemberList);
+        }
+    }
+}
+
