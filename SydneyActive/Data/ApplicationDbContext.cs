d3afc76a028881525e5b842e3c6d56111ada1d81
Chethz <chethzz@hotmail.com>
2022-06-24 14:13:42 +1000

    Added Entity Freamwork config and connection string

---
+++ SydneyActive/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SydneyActive.Models;
+
+namespace SydneyActive.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Member> Members { get; set; }
+    }
+}
+
