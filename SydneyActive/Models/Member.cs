fbb4effb00d86242b084aaaa05f28cbce291a180
Chethz <chethzz@hotmail.com>
2022-06-24 13:50:36 +1000

    Added member Model

---
+++ SydneyActive/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SydneyActive.Models
+{
+    public class Member
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public string ContactNumber { get; set; }
+    }
+}
+
